6e9ed432e5da584c75a06a162cdd4aa8d2d31c18
dmytromaimesko <dima.maimesko@gmail.com>
2026-03-26 00:03:00 +0200

    easy admin added

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Admin/VictoriousPuppyCrudController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\VictoriousPuppy;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class VictoriousPuppyCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return VictoriousPuppy::class;
+    }
+
+
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
